24e2aa79e0046bdf859b3d58a7badec93dd090be
Gain Chang <gain620@naver.com>
2014-05-18 16:38:30 +0900

    Initial Commit (05/18/2014)

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
+
+public class Main {
+	public static void main(String[] args){
+		
+		try {
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
